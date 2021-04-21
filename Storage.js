a3756dc23ef6611600bccba7c531781db56362b5
Angelo Breuer <angelobr@outlook.de>
2021-04-21 22:54:26 +0200

    Update

--- Storage.js
+++ Storage.js
@@ -1,11 +1,32 @@
 let citationsCache;
+function comparePersons(a, b, reverse) {
+    let result = (reverse ? a.lastName : a.firstName)
+        .localeCompare((reverse ? b.lastName : b.firstName));
+    if (result !== 0) {
+        return result;
+    }
+    result = (reverse ? a.firstName : a.lastName)
+        .localeCompare((reverse ? b.firstName : b.lastName));
+    return result;
+}
+function comparer(a, b) {
+    // compare authors
+    for (let index = 0; index < a.contributors.length, index < b.contributors.length; index++) {
+        let result;
+        if ((result = comparePersons(a.contributors[index], b.contributors[index], index === 0)) !== 0) {
+            return result;
+        }
+    }
+    // compare title
+    return a.title.localeCompare(b.title);
+}
 export function getCitations() {
     if (citationsCache) {
         return citationsCache;
     }
     const data = localStorage.getItem('data');
     citationsCache = ((data ? JSON.parse(data) : [])) || [];
-    citationsCache.sort((a, b) => a.title.localeCompare(b.title));
+    citationsCache.sort(comparer);
     return citationsCache;
 }
 export function saveCitations(value) {
